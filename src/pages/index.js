80fdf26acc3330c3903bf5f06816152756424a81
humza1997 <65232658+humza1997@users.noreply.github.com>
2021-09-06 20:56:19 +0100

    Delete src directory

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Home } from './Home';
-export { default as Pins } from './Pins';
-export { default as Lists } from './Lists';
-export { default as Map } from './Map';
-export { default as Share } from './Share';
-export { default as Profile } from './Profile';
-export { default as Error404 } from './Error404';
