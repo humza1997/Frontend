1005d933c33fe39d6d63675594bfad5842a936b7
Shav <shavhugan@gmail.com>
2021-09-06 23:29:09 +0100

    added shuffle function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 import axios from 'axios';
+
+//Helper Shuffle function:
+export const shuffle = (arr) => {
+    let c = arr.length;
+    let rand;
+    while (c !== 0) {
+        rand = Math.floor(Math.random() * c);
+        c--;
+        [arr[c], arr[rand]] = [arr[rand], arr[c]];
+    }
+    return arr;
+};
