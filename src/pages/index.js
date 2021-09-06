5cb8f569c147307a507c12a3e9dd3f92a274d9f3
Shav <shavhugan@gmail.com>
2021-09-06 10:56:23 +0100

    exported Pins, Lists, Map, Share, Profile in pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,6 @@
 export { default as Home } from './Home';
+export { default as Pins } from './Pins';
+export { default as Lists } from './Lists';
+export { default as Map } from './Map';
+export { default as Share } from './Share';
+export { default as Profile } from './Profile';
