4730f9db47daf73a3077be778823b865dde277de
Shav <shavhugan@gmail.com>
2021-09-07 11:18:59 +0100

    add button component

---
+++ src/components/AddButton/index.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from "react";
+import "./style.css";
+import { NavLink } from "react-router-dom";
+
+const AddButton = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const postData = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      password: e.target.messageShav.value,
+    };
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch("/api/*", options)
+      .then((r) => r.json())
+      .then(() => e.target.reset())
+      .catch(console.warn);
+  };
+
+  return <i id='addButton' class='fas fa-plus-circle fa-3x'></i>;
+};
+
+export default AddButton;
