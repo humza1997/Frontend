976f2a18ef0c4e96e4e6719112950917007ec060
Shav <shavhugan@gmail.com>
2021-09-07 11:20:21 +0100

    action creators were added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,38 @@
 import axios from 'axios';
+export const addList = (list) => ({
+    type: "ADD_LIST",
+    payload: list,
+});
+
+export const addUsername = (username) => ({
+    type: "ADD_USERNAME",
+    payload: username,
+});
+
+export const resetState = () => ({
+    type: "RESET",
+    payload: [0, " ", 0],
+});
+
+// export const loadList = (id) => {
+//   return async (dispatch) => {
+//     try {
+//       const { data } = await axios.get(`localhost:8080/lists/${id}`);
+
+//       dispatch({
+//         type: "LOAD_LIST",
+//         payload: data,
+//       });
+//     } catch (err) {
+//       console.warn(err.message);
+//       dispatch({
+//         type: "SET_ERROR",
+//         payload: err.message,
+//       });
+//     }
+//   };
+// };  
+
 
 //Helper Shuffle function:
 export const shuffle = (arr) => {
