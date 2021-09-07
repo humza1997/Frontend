645a4e9aa1460a02d1f39f6b729c440d3d58e4e2
Shav <shavhugan@gmail.com>
2021-09-07 11:21:03 +0100

    pages exports update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 export { default as Home } from './Home';
 export { default as Pins } from './Pins';
-export { default as Lists } from './Lists';
 export { default as Map } from './Map';
 export { default as Share } from './Share';
 export { default as Profile } from './Profile';
 export { default as Error404 } from './Error404';
+export { default as ListIndex } from "./ListIndex";
+export { default as List } from "./List";
+export { default as PinsIndex } from "./PinsIndex";
