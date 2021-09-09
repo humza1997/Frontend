15befb14df4d75ec038037b8edbc2705de998cee
Shav <shavhugan@gmail.com>
2021-09-09 09:38:59 +0100

    moved link in location card

--- src/components/LocationCard/index.js
+++ src/components/LocationCard/index.js
@@ -4,15 +4,15 @@ import { Link, useParams } from "react-router-dom";
 
 const LocationCard = ({ name, id, iconClass, border }) => {
   return (
-    <Link to={`/pins/${id}`}>
-      <div className='rounded-lg border-2 px-5 my-5' style={{ borderColor: `${border}` }}>
-        <div className='flex flex-row justify-between mt-3 mb-2'>
-          <h1>{name}</h1>
-          <p className='hidden'>{id}</p>
-          <i className={iconClass}></i>
-        </div>
+
+    <div className='rounded-lg border-2 px-5 my-5' style={{ borderColor: `${border}` }}>
+      <div className='flex flex-row justify-between mt-3 mb-2'>
+        <Link to={`/pins/${id}`}><h1>{name}</h1></Link>
+        <p className='hidden'>{id}</p>
+        <i className={iconClass}></i>
       </div>
-    </Link>
+    </div>
+
   );
 };
 
